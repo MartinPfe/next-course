8b96af55ddae85b5dae4f8e8cb9a81d15d298752
Martin <martin.pfe@gmail.com>
2025-09-09 17:23:06 -0300

    Auth 0 login

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,4 +1,5 @@
 import NavLinks from '@/app/ui/dashboard/nav-links';
+import Profile from './profile';
  
 export default function SideNav() {
   return (
@@ -6,6 +7,9 @@ export default function SideNav() {
       // ...
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
+        <a href="/auth/login">Login</a>
+        <a href="/auth/logout">Logout</a>
+        <Profile />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
       </div>
     </div>
